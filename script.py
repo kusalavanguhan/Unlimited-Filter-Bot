0a7775697e96ec617330b9e1a79afe52bb0b9595
kusalavanguhan <84655605+kusalavanguhan@users.noreply.github.com>
2021-05-23 11:41:24 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,10 +2,7 @@ class Script(object):
 
     START_MSG = """<b>Hy {},
 
-I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
-
-See <i>/help</i> for commands and more details.</b>
+Not for u only @Tamil_Tentkotta
 """
 
 
@@ -48,13 +45,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @Tamil_Tentkotta</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @Tamil_Tentkotta   
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
