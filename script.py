fa1569c793ebb08863fb8c16e78607b5a40c0ab9
kusalavanguhan <84655605+kusalavanguhan@users.noreply.github.com>
2021-05-23 12:00:50 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ Not for u only @Tamil_Tentkotta
 
 
     HELP_MSG = """
-<i>Add me as admin in your group and start filtering :)</i>
+
 
 
 <b>Basic Commands;</b>
@@ -15,36 +15,7 @@ Not for u only @Tamil_Tentkotta
 /start - Check if I'm alive!
 /help - Command help
 /about - Something about me!
-
-
-<b>Filter Commands;</b>
-
-<code>/add name reply</code>  -  Add filter for name
-
-<code>/del name</code>  -  Delete filter
-
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
-<code>/viewfilters</code>  -  List all filters in chat
-
-
-<b>Connection Commands;</b>
-
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-
-<code>/connections</code>  -  Manage your connections.
-
-
-<b>Extras;</b>
-
-/status  -  Shows current status of your bot (Auth User Only)
-
 /id  -  Shows ID information
-
-<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
-
 <b>© @Tamil_Tentkotta</b>
 """
 
